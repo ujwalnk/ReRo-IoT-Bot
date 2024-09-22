9a1bf3af0e2c3985384f487b3506bca1fad85e22
Ujwal N K <ujwal.nk@gmail.com>
2024-09-22 23:31:09 +0530

    Initial Commit Header file ith documentation

---
+++ header.py
@@ -0,0 +1,79 @@
+# TODO: Remove the below TEST imports
+
+import random
+
+# TODO: Remove the above TEST imports
+
+
+def set_speed(right_speed, left_speed):
+    """
+    Function to set the speed of the motors
+    
+    @param:
+        right_speed: int - Speed of the right motor 
+        left_speed: int - Speed of the left motor
+
+    @return:
+        bool - Speed set success or failure
+
+    right_speed & left_speed should be a number between -100 and 100,
+    speed < 0 : moving backwards
+    speed = 0 : stop
+    speed > 0 : moving forward
+
+    
+    """
+
+    if not (-100 <= right_speed <= 100 and -100 <= left_speed <= 100):
+        return False
+    
+    # TODO: Code to set the motor speed goes here
+    return True
+    
+
+
+def get_speed():
+    """
+    Function to get the speed of the motors
+    
+    @return:
+        right_speed: int - Speed of the right motor 
+        left_speed: int - Speed of the left motor
+
+    right_speed & left_speed will be a number between -100 and 100,
+    speed < 0 : moving backwards
+    speed = 0 : stop
+    speed > 0 : moving forward
+
+    """
+
+    # TODO: Code to reading the current motor speed
+    return [random.randint(-100, 100), random.randint(-100, 100)]
+
+
+def get_distance_to_obstacle():
+    """
+    Function to get the distance to the obstacle in front using the IR sensor
+    
+    @return:
+        distance: float - Distance to the nearest obstacle in cm
+
+    """
+
+    # TODO: Code to getting the IR Sensor distance
+    return random.randint(0, 100)
+
+def get_color_grid():
+    """
+    Function to get the color values of the sensor array
+    
+    @return:
+        color: [bool, bool, bool, bool, bool] - Ground color white or black
+
+        white will be represented by a value of True, 
+        and black will be represented by the value False
+
+    """
+
+    return [random.choice([True, False]) for _ in range(5)]
+
